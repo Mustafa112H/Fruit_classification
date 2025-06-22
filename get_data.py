cb4f7e1b3774246332baacc09eb7dd8d8f24f212
Mustafa112H <hebamustafa1112@gmail.com>
2025-06-22 05:19:30 +0300

    ok

--- get_data.py
+++ get_data.py
@@ -3,14 +3,14 @@ from icrawler.builtin import BingImageCrawler
 import os
 
 #Folder name
-os.makedirs('pomegranates', exist_ok=True)
+os.makedirs('Data/banana', exist_ok=True)
 
-keyword = 'fresh promagrante'
+keyword = 'banana no back'
 
-bing_crawler = BingImageCrawler(storage={'root_dir': 'pomegranates'})
+bing_crawler = BingImageCrawler(storage={'root_dir': 'Data/banana'})
 bing_crawler.crawl(keyword=keyword, max_num=300, file_idx_offset=1900)
 
 
 
-crawler2 = GoogleImageCrawler(storage={'root_dir': 'pomegranates'})
+crawler2 = GoogleImageCrawler(storage={'root_dir': 'Data/banana'})
 crawler2.crawl(keyword=keyword, max_num=300, file_idx_offset=900)
